89613abb0340ed77188ef49fd6758dab295d66a8
Denis Sanches <denis.a.sanches@gmail.com>
2020-10-31 12:03:05 -0400

    update GraphQL schema with Issues

--- server/schema/typeDefs.js
+++ server/schema/typeDefs.js
@@ -7,7 +7,22 @@ module.exports.typeDefs = gql`
     email: String
   }
 
+  type Issues {
+    id: ID!
+    description: String
+    reported_count: Float
+    task_owner: String
+    username: String
+    user_id: Float
+    upvotes_count: Float
+    resolution_status: Float
+    confirm_resolved_count: Float
+    date_issued: String
+    date_marked_in_progress: String
+    date_marked_resolved: String
+  }
+
   type Query {
-      getAll: [User]
+    getAll: [User]
   }
 `;
