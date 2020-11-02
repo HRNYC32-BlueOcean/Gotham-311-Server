9b03a01cf1a771dc60edf10ef5848480660cec38
Denis Sanches <denis.a.sanches@gmail.com>
2020-11-02 17:04:29 -0500

    update for Heroku deployment

--- db/index.js
+++ db/index.js
@@ -1,15 +1,17 @@
 require('dotenv').config();
 const { Sequelize } = require('sequelize');
 
-const { DB_USERNAME, DB_PASSWORD, DB_NAME, DB_PORT, DB_HOST } = process.env;
+// const { DB_USERNAME, DB_PASSWORD, DB_NAME, DB_PORT, DB_HOST } = process.env;
 
-const db = new Sequelize(DB_NAME, DB_USERNAME, DB_PASSWORD, {
-  host: DB_HOST,
-  dialect: 'postgres',
-  define: {
-    freezeTableName: true,
-  },
-});
+// const db = new Sequelize(DB_NAME, DB_USERNAME, DB_PASSWORD, {
+//   host: DB_HOST,
+//   dialect: 'postgres',
+//   define: {
+//     freezeTableName: true,
+//   },
+// });
+
+const sequelize = new Sequelize(process.env.DATABASE_URL);
 
 db.authenticate()
   .then(() => {
