dc2a1559060abbe0062806bbd72519ac670a460b
Denis Sanches <denis.a.sanches@gmail.com>
2020-11-02 17:09:22 -0500

    bug fix for Heroku deployment

--- db/index.js
+++ db/index.js
@@ -11,7 +11,7 @@ const { Sequelize } = require('sequelize');
 //   },
 // });
 
-const sequelize = new Sequelize(process.env.DATABASE_URL);
+const db = new Sequelize(process.env.DATABASE_URL);
 
 db.authenticate()
   .then(() => {
