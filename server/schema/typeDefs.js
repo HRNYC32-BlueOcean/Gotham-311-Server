56465a92f6d06f154025110ad07668e30e4ae22f
Denis Sanches <denis.a.sanches@gmail.com>
2020-11-02 16:42:04 -0500

    implement issues mutations

--- server/schema/typeDefs.js
+++ server/schema/typeDefs.js
@@ -38,6 +38,42 @@ module.exports.typeDefs = gql`
 
   type Mutation {
     createUser(name: String!, email: String!, phone: String!): User
-    updateUser(id: ID!, name: String, email: String, phone: String): User
+    updateUser(id: ID!, name: String, email: String, phone: String): [Float]
+    deleteUser(id: ID!): Float
+
+    createIssue(
+      type: String
+      title: String
+      description: String
+      reported_count: Float
+      task_owner: String
+      user_id: Float
+      lat: Float
+      lng: Float
+      upvotes_count: Float
+      resolution_status: Float
+      confirm_resolved_count: Float
+      date_marked_in_progress: String
+      date_marked_resolved: String
+    ): Issue
+
+    updateIssue(
+      id: ID!
+      type: String
+      title: String
+      description: String
+      reported_count: Float
+      task_owner: String
+      user_id: Float
+      lat: Float
+      lng: Float
+      upvotes_count: Float
+      resolution_status: Float
+      confirm_resolved_count: Float
+      date_marked_in_progress: String
+      date_marked_resolved: String
+    ): [Float]
+
+    deleteIssue(id: ID!): Float
   }
 `;
